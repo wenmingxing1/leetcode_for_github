3c6fb4b050d8374555a75832a36a42a5d69286f3
wenmingxing1 <1793701192@qq.com>
2018-01-14 21:09:15 +0800

    重构二叉树（前序和中序）

---
+++ 105_ConstructBinaryTreeFromPreorderAndInorder.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int length = preorder.size();
+        if (preorder.empty() || inorder.empty())
+            return nullptr;
+        int value = preorder[0];
+        TreeNode* root = new TreeNode(value);
+
+        //找到中序中根结点的位置
+        int indexRoot;
+        for (int i = 0; i < length; ++i) {
+            if (inorder[i] == value)
+            {
+                indexRoot = i;
+                break;
+            }
+
+        }
+
+        //各自分开左右子树
+        vector<int> preLeft;
+        vector<int> preRight;
+        vector<int> inLeft;
+        vector<int> inRight;
+
+        for (int i = 0; i < indexRoot; ++i) {
+            preLeft.push_back(preorder[i+1]);
+            inLeft.push_back(inorder[i]);
+        }
+
+        for (int i = indexRoot+1; i < length; ++i) {
+            preRight.push_back(preorder[i]);
+            inRight.push_back(inorder[i]);
+        }
+
+        root->left = buildTree(preLeft, inLeft);
+        root->right = buildTree(preRight, inRight);
+
+        return root;
+
+    }
+};
