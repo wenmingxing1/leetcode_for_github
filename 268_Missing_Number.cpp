146298def5f4c7cb7419178258cfdcc69c6e4dd1
wenmingxing1 <1793701192@qq.com>
2018-03-13 22:08:01 +0800

    缺失数字

---
+++ 268_Missing_Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int length = nums.size();
+        for (int i = 0; i < length; ++i) {
+            while (nums[i] != i && nums[i] != length)
+                swap(nums[i], nums[nums[i]]);
+        }
+
+        for (int i = 0; i < length; ++i) {
+            if (nums[i] != i)
+                return i;
+        }
+
+        return length;
+
+
+    }
+};
