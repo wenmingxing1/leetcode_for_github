338f90ce79e869ca5e7fae14f8204129cb53e3b8
wenmingxing1 <1793701192@qq.com>
2018-05-04 13:09:32 +0800

    括号匹配

--- 20_ValidParentheses.cpp
+++ 20_ValidParentheses.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//使用栈的方法
 class Solution{
 public:
     bool isValid(const string& str) {
@@ -23,12 +24,29 @@ public:
     }
 };
 
+//不用栈的方法
+class  Solution2 {
+public:
+    bool isValid(const string& str) {
+        if (str.size() == 0) return true;
+        int res = 0;
+        for (auto ch:str) {
+            if (ch == '{' || ch == '(' || ch == '[') res+=1;
+            else if (ch == '}' || ch == ')' || ch == ']') res-=1;
+            else res+=0;
+
+            if (ch < 0) return false;
+        }
+        return res == 0;
+    }
+};
+
 int main() {
     string strTest1 = "{}";
     string strTest2 = "[";
     string strTest3 = "[{}]()";
 
-    Solution s;
+    Solution2 s;
     cout << s.isValid(strTest1) << " " << s.isValid(strTest2) << " " << s.isValid(strTest3) << endl;
 
     return 0;
