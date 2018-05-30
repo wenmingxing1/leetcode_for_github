f43c31570bf835fa2b84d8b58ea176c6ecf953a1
wenmingxing1 <1793701192@qq.com>
2018-05-30 15:30:59 +0800

    动态规划处理杨辉三角最小路径

--- 120_Triangle.cpp
+++ 120_Triangle.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-class Solution {
+//动态规划方法，但是空间可以继续优化
+class Solution1 {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int res = 0;
@@ -35,20 +36,40 @@ public:
         for (int i = 0; i < row; ++i) {
             cout << dp[row-1][i] << " ";
         }
+        cout << endl;
 
 
         return res;
     }
 };
 
+//采用从下向上的计算方法
+class Solution2 {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        for (int i = triangle.size() - 2; i >= 0; --i) {
+            for (int j = 0; j <= i; ++j) {
+                triangle[i][j] += min(triangle[i+1][j+1], triangle[i+1][j]);
+            }
+        }
+        return triangle[0][0];
+    }
+};
+
 int main() {
-    Solution solu;
+    Solution1 solu1;
+
     vector<vector<int>> test1 = { {-1}, {2,3}, {1,-1,-3} };
     vector<vector<int>> test2 = { {2}, {3,4}, {6,5,7}, {4,1,8,3} };
     vector<vector<int>> test3 = {{-2},{5,2},{-3,2,7},{8,7,2,-8},{7,5,3,8,-1},{-7,-2,-2,0,-1,6},{4,7,7,-9,9,8,2},{-5,-8,-1,-1,6,7,3,-5},{4,1,-8,-7,-1,5,7,-8,3},{-7,-8,-8,0,1,-3,-4,-8,-6,9},{-7,-9,5,1,-8,6,9,0,-1,6,9},{0,-3,-3,-2,-1,6,-7,-7,4,3,-5,2}};
-    cout << solu.minimumTotal(test1) << endl;
-    cout << solu.minimumTotal(test2) << endl;
-    cout << solu.minimumTotal(test3) << endl;
+    cout << solu1.minimumTotal(test1) << endl;
+    cout << solu1.minimumTotal(test2) << endl;
+    cout << solu1.minimumTotal(test3) << endl;
+
+    Solution2 solu2;
+    cout << solu2.minimumTotal(test1) << endl;
+    cout << solu2.minimumTotal(test2) << endl;
+    cout << solu2.minimumTotal(test3) << endl;
 
     return 0;
 }
