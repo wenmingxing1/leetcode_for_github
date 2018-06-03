73478c142fc8dedc34d2f608ea4767797fea4aec
wenmingxing1 <1793701192@qq.com>
2018-06-03 20:29:31 +0800

    类3sum

--- 16_3SumClosest.cpp
+++ 16_3SumClosest.cpp
@@ -13,16 +13,34 @@ public:
         sort(nums.begin(), nums.end());
 
         int res = nums[0]+nums[1]+nums[2];
+        int diff = abs(res - target);
 
-        for (int i = 0; i < length; ++i) {
-            int temp = target - nums[i];
+        for (int i = 0; i < length-2; ++i) {
 
+            int left = i+1;
+            int right = length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                int newdiff = abs(sum - target);
+                if (newdiff < diff) {
+                    diff = newdiff;
+                    res = sum;
+                }
+
+                if (sum < target) ++left;
+                else --right;
+            }
         }
-
-    }
-
-private:
-    int twoSumClosest(vector<int>& nums, int start, int end, int target) {
-
+        return res;
     }
 };
+
+int main() {
+    vector<int> vec = {-1,2,1,-4};
+    Solution solu;
+
+    cout << solu.threeSumClosest(vec,1);
+
+    return 0;
+}
