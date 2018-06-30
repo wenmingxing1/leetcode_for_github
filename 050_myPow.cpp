e1dc9ecaad3b0e9d73f7414da0a06465ac8933be
wenmingxing1 <1793701192@qq.com>
2018-06-30 12:07:43 +0800

    Rename 50_myPow.cpp to 050_myPow.cpp

--- 050_myPow.cpp
+++ 050_myPow.cpp
@@ -1,10 +1,10 @@
-//½£Ö¸offer11ÌâÔ­Ìâ£¬µ«ÊÇLeetCodeÉÏĞèÒª´¦ÀíintµÄ±ß½çÎÊÌâ
+//å‰‘æŒ‡offer11é¢˜åŸé¢˜ï¼Œä½†æ˜¯LeetCodeä¸Šéœ€è¦å¤„ç†intçš„è¾¹ç•Œé—®é¢˜
 class Solution {
 public:
     double myPow(double x, int n) {
         double res = 1.0;
         if (n < 0) {
-            if (n == INT_MIN) return 1/(Pow_nor(x, INT_MAX)*x);     //ÕâÊÇÎªÁË´¦ÀíÕâÖÖÌØÊâÇé¿ö£¬INT_MIN=-2147483648£»INT_MAX=2147483647
+            if (n == INT_MIN) return 1/(Pow_nor(x, INT_MAX)*x);     //è¿™æ˜¯ä¸ºäº†å¤„ç†è¿™ç§ç‰¹æ®Šæƒ…å†µï¼ŒINT_MIN=-2147483648ï¼›INT_MAX=2147483647
             else return 1/Pow_nor(x, -n);
         }
         else
@@ -15,7 +15,7 @@ public:
     }
 
 private:
-    //ÅĞ¶ÏÁ½¸ödoubleÊÇ·ñÏàµÈ
+    //åˆ¤æ–­ä¸¤ä¸ªdoubleæ˜¯å¦ç›¸ç­‰
     bool equal(double i, double j) {
         if ( (i - j <= 0.0000001) && (i - j >= -0.0000001) )
             return true;
@@ -23,14 +23,14 @@ private:
             return false;
     }
 
-    //¼Ù¶¨nÎªÕıµÄÒ»°ãPow
+    //å‡å®šnä¸ºæ­£çš„ä¸€èˆ¬Pow
     double Pow_nor(double x, int n) {
-        if (n == 0)     //Á½¸öµİ¹é½áÊøÌõ¼ş
+        if (n == 0)     //ä¸¤ä¸ªé€’å½’ç»“æŸæ¡ä»¶
             return 1.0;
         if (n == 1)
             return x;
 
-        double result = Pow_nor(x, n>>1);   //µİ¹éµ÷ÓÃPow_nor
+        double result = Pow_nor(x, n>>1);   //é€’å½’è°ƒç”¨Pow_nor
         result *= result;
         if (n & 0x01)
             result *= x;
