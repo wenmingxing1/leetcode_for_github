bf45a55742b9f38beb41608197fdd4edcff36b4c
wenmingxing1 <1793701192@qq.com>
2018-07-25 20:30:08 +0800

    数字解码，动态规划求解，类斐波那契数列

--- 091_DecodeWays.cpp
+++ 091_DecodeWays.cpp
@@ -15,10 +15,11 @@ public:
         dp[1] = 1;
 
         for (int i = 1; i < length; ++i) {
-            if (s[i] == '0') dp[i] = 0;
+            if (s[i] == '0') dp[i] = 0;     //只有一个单独为0，则是不能翻译的
 
+            //如果能和前一个组成小于26的数，则包含单独翻译，以及加上前一个翻译
             if (s[i-1] == '1' || (s[i-1] <= '2' && s[i] <= '6')) dp[i+1] = dp[i]+dp[i-1];
-            else dp[i+1] = dp[i];
+            else dp[i+1] = dp[i];   //否则就只能单独翻译
         }
         return dp[length];
     }
